6476f1c158a6378a6362f7feeac144bdc877540e
sarathorn11 <raths8546@gmail.com>
2024-01-11 22:50:26 +0700

    add js

--- js/home.js
+++ js/home.js
@@ -100,20 +100,216 @@ const todayDeal = [
         DealOfDay : "Deal of the Day",
         desc : "Vivo Y75 - 18W fast charge, FHD plus display"
     },
+
+]
+
+const todayDeal2 = [
     {
-        img : "https://m.media-amazon.com/images/I/41wyFlObIrL._AC_SY200_.jpg",
-        discount : "25",
+        img : "https://m.media-amazon.com/images/I/317lhW5iHVL._AC_SY200_.jpg",
+        discount : 65,
         DealOfDay : "Deal of the Day",
-        desc : "Nokia 5710 - newly launched 4G feature phone"
+        desc : "Powerbank from Mi, Ambrane, URBN and more"
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/41yxd6A+ZAL._AC_SY200_.jpg",
+        discount : 65,
+        DealOfDay : "Deal of the Day",
+        desc : "Top deals on Truke, Govo and more"
     },
     {
-        img : "https://m.media-amazon.com/images/I/31Zq-alRH9L._AC_SY200_.jpg",
+        img : "https://m.media-amazon.com/images/I/41wcMkODJLL._AC_SY200_.jpg",
+        discount : 75,
+        DealOfDay : "Deal of the Day",
+        desc : "Amazing deals on pTron, Jabra, Portronics"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/61QQtY6qtHL._AC_SY200_.jpg",
+        discount : 82,
+        DealOfDay : "Deal of the Day",
+        desc : "Levi's, Allen Solly, ANNI DESIGNER, Janasya & more"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31aTPRvXiSL._AC_SY200_.jpg",
+        discount : 77,
+        DealOfDay : "Deal of the Day",
+        desc : "Exciting  deals on Mivi, Wecool, Wings"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41Diz41FkhL._AC_SY200_.jpg",
         discount : 32,
         DealOfDay : "Deal of the Day",
-        desc : "Vacuum Cleaners from Top Brands"
+        desc : " Grand Gaming Days offers on Bestselling Gaming Laptops & Desktops - Exchange & No Cost EMI Available"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41DjFnGQ1FL._AC_SY200_.jpg",
+        discount : 31,
+        DealOfDay : "Deal of the Day",
+        desc : "High Performance Handpicked Desktop Computers from HP, Lenovo and more"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31aNgbvYJKL._AC_SY200_.jpg",
+        discount : 66,
+        DealOfDay : "Deal of the Day",
+        desc : "Best Prices on boAt Headphones, Soundbars and Speakers"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31G1NouVxaL._AC_SY200_.jpg",
+        discount : 80,
+        DealOfDay : "Deal of the Day",
+        desc : "Jaw dropping deals on headsets"
     },
 
+    {
+        img : "https://m.media-amazon.com/images/I/31EXkIBVKUL._AC_SY200_.jpg",
+        discount : 50,
+        DealOfDay : "Deal of the Day",
+        desc : "adidas & campus Footwear"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/416x-scGWgL._AC_SY200_.jpg",
+        discount : 64,
+        DealOfDay : "Deal of the Day",
+        desc : "Grand Gaming Days Offers on Accessories and Storage Devices"
+    },
 
+    {
+        img : "https://m.media-amazon.com/images/I/31VfkewLnlL._AC_SY200_.jpg",
+        discount : 54,
+        DealOfDay : "Deal of the Day",
+        desc : "cooking essential"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/91kKqnkv9jL._AC_SY200_.jpg",
+        discount : 52,
+        DealOfDay : "Deal of the Day",
+        desc : "TOP Deals on PUMA & Skechers footwear"
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/41fsUIG2fwL._AC_SY200_.jpg",
+        discount : 12,
+        DealOfDay : "Deal of the Day",
+        desc : "Handpicked Intel Powered Laptops; High Performance"
+    },
+   
+    {
+        img : "https://m.media-amazon.com/images/I/21wNUazPKRL._AC_SY200_.jpg",
+        discount : 20,
+        DealOfDay : "Deal of the Day",
+        desc : "Never before deals on HP printers"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41bFnhrsyOL._AC_SY200_.jpg",
+        discount : 19,
+        DealOfDay : "Deal of the Day",
+        desc : "Vivo Y75 - 18W fast charge, FHD plus display"
+    },
+
+]
+const todayDeal3 = [
+    {
+        img : "https://m.media-amazon.com/images/I/31aTPRvXiSL._AC_SY200_.jpg",
+        discount : 77,
+        DealOfDay : "Deal of the Day",
+        desc : "Exciting  deals on Mivi, Wecool, Wings"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/21wNUazPKRL._AC_SY200_.jpg",
+        discount : 20,
+        DealOfDay : "Deal of the Day",
+        desc : "Never before deals on HP printers"
+    },
+    
+    {
+        img : "https://m.media-amazon.com/images/I/41wcMkODJLL._AC_SY200_.jpg",
+        discount : 75,
+        DealOfDay : "Deal of the Day",
+        desc : "Amazing deals on pTron, Jabra, Portronics"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/61QQtY6qtHL._AC_SY200_.jpg",
+        discount : 82,
+        DealOfDay : "Deal of the Day",
+        desc : "Levi's, Allen Solly, ANNI DESIGNER, Janasya & more"
+    },
+    
+    {
+        img : "https://m.media-amazon.com/images/I/41Diz41FkhL._AC_SY200_.jpg",
+        discount : 32,
+        DealOfDay : "Deal of the Day",
+        desc : " Grand Gaming Days offers on Bestselling Gaming Laptops & Desktops - Exchange & No Cost EMI Available"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41bFnhrsyOL._AC_SY200_.jpg",
+        discount : 19,
+        DealOfDay : "Deal of the Day",
+        desc : "Vivo Y75 - 18W fast charge, FHD plus display"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41DjFnGQ1FL._AC_SY200_.jpg",
+        discount : 31,
+        DealOfDay : "Deal of the Day",
+        desc : "High Performance Handpicked Desktop Computers from HP, Lenovo and more"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31aNgbvYJKL._AC_SY200_.jpg",
+        discount : 66,
+        DealOfDay : "Deal of the Day",
+        desc : "Best Prices on boAt Headphones, Soundbars and Speakers"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31G1NouVxaL._AC_SY200_.jpg",
+        discount : 80,
+        DealOfDay : "Deal of the Day",
+        desc : "Jaw dropping deals on headsets"
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/31EXkIBVKUL._AC_SY200_.jpg",
+        discount : 50,
+        DealOfDay : "Deal of the Day",
+        desc : "adidas & campus Footwear"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/416x-scGWgL._AC_SY200_.jpg",
+        discount : 64,
+        DealOfDay : "Deal of the Day",
+        desc : "Grand Gaming Days Offers on Accessories and Storage Devices"
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/31VfkewLnlL._AC_SY200_.jpg",
+        discount : 54,
+        DealOfDay : "Deal of the Day",
+        desc : "cooking essential"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/91kKqnkv9jL._AC_SY200_.jpg",
+        discount : 52,
+        DealOfDay : "Deal of the Day",
+        desc : "TOP Deals on PUMA & Skechers footwear"
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/41fsUIG2fwL._AC_SY200_.jpg",
+        discount : 12,
+        DealOfDay : "Deal of the Day",
+        desc : "Handpicked Intel Powered Laptops; High Performance"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/317lhW5iHVL._AC_SY200_.jpg",
+        discount : 65,
+        DealOfDay : "Deal of the Day",
+        desc : "Powerbank from Mi, Ambrane, URBN and more"
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/41yxd6A+ZAL._AC_SY200_.jpg",
+        discount : 65,
+        DealOfDay : "Deal of the Day",
+        desc : "Top deals on Truke, Govo and more"
+    },
 
 ]
 
@@ -182,9 +378,8 @@ setInterval(renderSlideAuto, 2000);
 
 
 //today deals 
-console.log(todayDeal)
 let todayDealProductListEl = document.querySelector(".today_deals_product_list")
-console.log(todayDealProductListEl)
+
 
 
 let todayDealProductHTML = ""
@@ -212,6 +407,74 @@ for (let i = 0; i < todayDeallength; i++) {
 }
 
 todayDealProductListEl.innerHTML = todayDealProductHTML
+// =========================================================================
+let todayDealProductListEl1 = document.querySelector(".today_deals_product_list1")
+
+
+
+let todayDealProductHTML1 = ""
+
+let todayDeallength1 = todayDeal3.length
+
+for (let i = 0; i < todayDeallength; i++) {
+    // console.log(todayDeal[i])
+
+    todayDealProductHTML1 += `
+        <div class="today_deals_product_item1">
+                <div class="todayDeals_product_image">
+                    <img src=${todayDeal3[i].img} />
+                </div>
+            
+
+
+            <div class="discount_Contaienr">
+                <a href="#">Up to ${todayDeal3[i].discount}% off</a>
+                <a href="#">${todayDeal3[i].DealOfDay}</a>
+            </div>
+            <p>${todayDeal3[i].desc}</p>
+        </div>
+    `
+}
+
+todayDealProductListEl1.innerHTML = todayDealProductHTML1
+// // =====================================================================
+let todayDealProductListEl2 = document.querySelector(".today_deals_product_list2")
+
+
+
+let todayDealProductHTML2 = ""
+
+let todayDeallength2 = todayDeal2.length
+
+for (let i = 0; i < todayDeallength2; i++) {
+    // console.log(todayDeal[i])
+
+    todayDealProductHTML2 += `
+        <div class="today_deals_product_item2">
+                <div class="todayDeals_product_image">
+                    <img src=${todayDeal2[i].img} />
+                </div>
+            
+
+
+            <div class="discount_Contaienr">
+                <a href="#">Up to ${todayDeal2[i].discount}% off</a>
+                <a href="#">${todayDeal2[i].DealOfDay}</a>
+            </div>
+            <p>${todayDeal2[i].desc}</p>
+        </div>
+    `
+}
+
+todayDealProductListEl2.innerHTML = todayDealProductHTML2
+
+
+
+
+
+
+
+
 
 let today_deal_btn_prevEl = document.getElementById("today_deal_btn_prev")
 let today_deal_btn_nextEl = document.getElementById("today_deal_btn_next")
@@ -243,6 +506,81 @@ today_deal_btn_nextEl.addEventListener("click", () => {
 
     today_deals_product_itemEl.forEach(el =>{
         el.style.transform = `translateX(${startProduct}%)`
+    })
+    
+    
+})
+
+// ------------------------------------------------------------
+let today_deal_btn_prevEl1 = document.getElementById("today_deal_btn_prev1")
+let today_deal_btn_nextEl1 = document.getElementById("today_deal_btn_next1")
+let today_deals_product_itemEl1 = document.querySelectorAll(".today_deals_product_item1")
+
+let startProduct1 = 0;
+
+
+today_deal_btn_prevEl1.addEventListener("click", () => {
+
+   
+    if(startProduct1 < 0){
+        startProduct1 += 500
+    }
+    if(startProduct1 > -2000){
+        today_deals_product_itemEl1.forEach(el =>{
+            el.style.transform = `translateX(${startProduct1}%)`
+        })
+    }
+
+})
+
+today_deal_btn_nextEl1.addEventListener("click", () => {
+    // alert("next")
+    
+    if(startProduct1 > -1500){
+        startProduct1 -= 500
+    }
+
+    today_deals_product_itemEl1.forEach(el =>{
+        el.style.transform = `translateX(${startProduct1}%)`
+    })
+    
+    
+})
+
+
+// -------------------------------------------------------------------------
+
+// ------------------------------------------------------------
+let today_deal_btn_prevEl2 = document.getElementById("today_deal_btn_prev2")
+let today_deal_btn_nextEl2 = document.getElementById("today_deal_btn_next2")
+let today_deals_product_itemEl2 = document.querySelectorAll(".today_deals_product_item2")
+
+let startProduct2 = 0;
+
+
+today_deal_btn_prevEl2.addEventListener("click", () => {
+
+   
+    if(startProduct2 < 0){
+        startProduct2 += 500
+    }
+    if(startProduct2 > -2000){
+        today_deals_product_itemEl2.forEach(el =>{
+            el.style.transform = `translateX(${startProduct2}%)`
+        })
+    }
+
+})
+
+today_deal_btn_nextEl2.addEventListener("click", () => {
+    // alert("next")
+    
+    if(startProduct2 > -1500){
+        startProduct2 -= 500
+    }
+
+    today_deals_product_itemEl2.forEach(el =>{
+        el.style.transform = `translateX(${startProduct2}%)`
     })
     
     
